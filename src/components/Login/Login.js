715201108416d01fb2ea0a266cc7e5034033eda0
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-10-27 17:10:26 +0700

    call API login by POST axios

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 import './Login.css';
 
 export default function Login() {
+  const [userName, setuserName] = useState('');
+  const [password, setpassword] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  
+    const user = {
+    userName: userName,
+    password: password
+    };
+    
+    console.log(user);
+
+    axios.post(`http://localhost:5000/user/login`, user )
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+}
 
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
         <h1 className="heading">Login</h1>
+        <form onSubmit={handleSubmit}>
         <div>
-          <input placeholder="Name" className="joinInput" type="text"/>
+          <input placeholder="userName" className="joinInput" type="text" onChange={(event) => setuserName(event.target.value)}/>
         </div>
         <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text"/>
+          <input placeholder="Password" className="joinInput mt-20" type="password" onChange={(event) => setpassword(event.target.value)}/>
         </div>
-        <Link>
+        
           <button className={'button mt-20'} type="submit">Login</button>
-        </Link>
+        
+        </form>
         <h2 className="footing">HuuKhiemNee 2020</h2>
       </div>
     </div>
