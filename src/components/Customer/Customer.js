3ed43437da69598ebd52bb0675cec6fc0b922f8f
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-11-03 12:00:16 +0700

    complete api for customer page

--- src/components/Customer/Customer.js
+++ src/components/Customer/Customer.js
@@ -1,23 +1,83 @@
 import React, { useState } from 'react';
 import { Link, withRouter } from "react-router-dom";
+import axios from 'axios';
 
 import './Customer.css';
 
 function Customer(props) {
 
+  const [report, setReport] = useState('');
+
+  if (localStorage.getItem('token'))
+  var token = "HKNee " + localStorage.getItem('token').substring(1,localStorage.getItem('token').length-1);
+
   const Booking = async event => {
     event.preventDefault();
+    
+    const order = {
+      "Shipper": "1a1941cb83beb06352357780cd6b2a62f4518ccf680eebc9b934bfbca29c1c6c",
+      "Value": "500"
+    }
+
+  await axios.post(`http://localhost:5000/order/create`, order, {
+    headers: {
+      'Authorization': `${token}`
+    }
+  } )
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+    console.log(res.data.ID);
+    localStorage.setItem('ID_order', JSON.stringify(res.data.ID))
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
   }
 
   const Cancel = async event => {
     event.preventDefault();
 
+    if(localStorage.getItem('ID_order'))
+    var ID = localStorage.getItem('ID_order');
+
+    await axios.put(`http://localhost:5000/order/${ID}/cancel`, {}, {
+      headers: {
+        'Authorization': `${token}`
+      }
+    })
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err)
+  })
   }
 
   const Report = async event => {
     event.preventDefault();
     
+    if(localStorage.getItem('ID_order'))
+    var ID = localStorage.getItem('ID_order');
+
+    const report_ = {
+      Report: report
+    }
+
+    await axios.put(`http://localhost:5000/order/${ID}/report`, report_, {
+      headers: {
+        'Authorization': `${token}`
+      }
+    })
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err)
+  })
   }
 
   return (
@@ -29,6 +89,9 @@ function Customer(props) {
     <form onSubmit={Cancel}>
       <button type="submit">Cancel</button>
     </form>
+    <div>
+          <input placeholder="Report" type="text" onChange={(event) => setReport(event.target.value)}/>
+    </div>
     <form onSubmit={Report}>
       <button type="submit">Report</button>
     </form>
