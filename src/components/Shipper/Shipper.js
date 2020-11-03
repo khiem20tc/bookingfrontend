a8f7c42bc9f9a4155fc788ba1a663e9cd61d423d
khiem20tc <53548789+khiem20tc@users.noreply.github.com>
2020-11-03 13:52:41 +0700

    complete API for shipper page

--- src/components/Shipper/Shipper.js
+++ src/components/Shipper/Shipper.js
@@ -1,11 +1,83 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
+import axios from 'axios';
 
 import './Shipper.css';
 
-export default function Shipper() {
+function Shipper(props) {
+
+  const [report, setReport] = useState('');
+  const [ID, setID] = useState('');
+  const [state, setState] = useState('');
+
+  if (localStorage.getItem('token'))
+  var token = "HKNee " + localStorage.getItem('token').substring(1,localStorage.getItem('token').length-1);
+
+ const SetState = async event => {
+   event.preventDefault();
+
+   const State = {
+     State: state
+   }
+
+   await axios.put(`http://localhost:5000/order/${ID}/setstate`, State, {
+    headers: {
+      'Authorization': `${token}`
+    }
+  })
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+ }
+
+  const Report = async event => {
+    event.preventDefault();
+    
+    // if(localStorage.getItem('ID_order'))
+    // var ID = localStorage.getItem('ID_order');
+
+    const report_ = {
+      Report: report
+    }
+
+    await axios.put(`http://localhost:5000/order/${ID}/report`, report_, {
+      headers: {
+        'Authorization': `${token}`
+      }
+    })
+  .then(res => {
+    console.log(res);
+    console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err)
+  })
+  }
 
   return (
+    <div>
     <p>This is shipper page</p>
+    <div>
+          <input placeholder="ID" type="text" onChange={(event) => setID(event.target.value)}/>
+    </div>
+    <div>
+          <input placeholder="State" type="text" onChange={(event) => setState(event.target.value)}/>
+    </div>
+    <form onSubmit={SetState}>
+      <button type="submit">SetState</button>
+    </form>
+    <div>
+          <input placeholder="Report" type="text" onChange={(event) => setReport(event.target.value)}/>
+    </div>
+    <form onSubmit={Report}>
+      <button type="submit">Report</button>
+    </form>
+    </div>
   );
 }
+
+export default withRouter(Shipper)
